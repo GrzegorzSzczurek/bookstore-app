a60b231b4b5db621a48af56a845f6232d546814b
wojcikpatryk <wojcik.patrykk@gmail.com>
2018-11-15 10:52:59 +0100

    Project begin

--- src/test/java/com/wojcik/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/wojcik/bookstore/BookstoreApplicationTests.java
@@ -4,10 +4,11 @@ import com.wojcik.bookstore.entities.Client;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(JUnit4.class)
 @SpringBootTest
 public class BookstoreApplicationTests {
 
