084d3038afeb5312261346236dc8b0745606c053
wojcikpatryk <wojcik.patrykk@gmail.com>
2018-12-12 21:53:20 +0100

    Merge remote-tracking branch 'origin/maly'
    # Conflicts:
    #	pom.xml
    #	src/main/resources/application.properties

--- src/test/java/com/wojcik/demo/DemoApplicationTests.java
+++ src/test/java/com/wojcik/demo/DemoApplicationTests.java
@@ -1,7 +1,12 @@
 package com.wojcik.demo;
 
+import com.wojcik.demo.entity.User;
+import com.wojcik.demo.service.UserService;
+import com.wojcik.demo.service.UserServiceImpl;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,28 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+    @Autowired
+    private UserService userService;
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void isAdminUsernameTaken() {
+        String username = "admin";
+
+        boolean isTaken = userService.isUsernameTaken(username);
+
+        Assert.assertTrue(isTaken);
+    }
+
+    @Test
+    public void isUserFirstNameSettingCorrectly() {
+        User user = new User();
+        user.setFirstName("Patryk");
+
+        Assert.assertTrue(user.getFirstName().equals("Patryk"));
     }
 
 }
